7b42663c63399e190b084ff3f9e244cd15b7b6e8
Zane Hitchcox <zwhitchcox@gmail.com>
2015-04-26 20:37:52 -0400

    omit dupes

--- script.js
+++ script.js
@@ -50,6 +50,7 @@ app.controller('Ctrl', ['$scope','$resource','$http', function($scope,$resource,
       }
     }
     function addVidIdToStorage (id) {
+      console.log('added')
       var ids
       if (localStorage['ids'] === null || localStorage['ids'] ===undefined
       || localStorage === '') {
