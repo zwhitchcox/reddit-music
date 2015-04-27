8d8792ef56b33814925676bf5f0e5621a361856c
Zane Hitchcox <zwhitchcox@gmail.com>
2015-04-26 21:06:48 -0400

    omit dupes

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ app.controller('Ctrl', ['$scope','$resource','$http', function($scope,$resource,
       $scope.vids = res.data.children.reduce(function(prev,cur) {
         if (/^https?:\/\/(www\.)?youtube/.test(cur.data.url)) {
           var id = getJsonFromUrl(cur.data.url.substr(30)).v
+          console.log(id,!~JSON.parse(localStorage["ids"]).indexOf(id))
           if (!~JSON.parse(localStorage["ids"]).indexOf(id) || !$scope.omitRedundancies) {
             $scope.permalinks.push({title:cur.data.title,uri:cur.data.permalink})
             prev.push(id)
